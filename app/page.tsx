7dac8b4b8988268c754391a57be7f1ff4fe84b6f
seanimayi <seanimayi@gmail.com>
2025-10-11 12:54:02 +0100

    environment cleaned

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,14 @@ export default function Home() {
     <>
       <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-5">
         <h1
-          className="text-3xl font-poppins font-bold
+          className="text-3xl font-poppins font-bold text-slate-800
         "
         >
           Welcome admin!!
         </h1>
-        <p>Click the button below to go to your dashboard.</p>
+        <p className="text-slate-800 font-inter font-semibold">
+          Click the button below to go to your dashboard.
+        </p>
         <button
           onClick={() => router.push("./admin")}
           className="h-[50px] w-[200px] text-white bg-gradient-to-tr from-blue-500 to-purple-600 rounded-lg mt-5 hover:from-blue-600 hover:to-purple-700 flex gap-5 items-center"
